9afb41deac1cea6b3cd726b8585ef37f43c915a9
kadir-zeren <k4dzeren@gmail.com>
2024-02-03 14:38:53 +0300

    if else

--- conditions.js
+++ conditions.js
@@ -1,11 +1,33 @@
 const calismaSuresi = prompt("calisma süresi");
 let maas = prompt("maasinizi giriniz");
 
+// if (calismaSuresi >= 10) {
+//   maas = maas * 1.1;
+//   console.log("zamli maasiniz:", maas);
+//   console.log(`zamli maasiniz: ${maas}`);
+// } else {
+//   console.log("Uzgunuz maasinaza zama yapamıyoruz", maas);
+// }
+// console.log("güle güle");
+
 if (calismaSuresi >= 10) {
-  maas = maas * 1.1;
-  console.log("zamli maasiniz:", maas);
+  maas = Math.round(maas * 1.1);
   console.log(`zamli maasiniz: ${maas}`);
-} else {
-  console.log("Uzgunuz maasinaza zama yapamıyoruz", maas);
 }
 console.log("güle güle");
+
+const grade = prompt("pleasse enter your grade");
+
+let result = "";
+if (grade < 40) {
+  result = " ff";
+} else if (grade <= 50) {
+  result = "DD";
+} else if (grade <= 65) {
+  result = "cc";
+} else if (grade <= 89) {
+  result = "bb";
+} else {
+  result = "AA";
+}
+console.log(`your score:${result}`);
